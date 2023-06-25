58dd9446b70a69f9f05ce1b38c776ba42db3e2ff
a-thomas-22 <athomas@offchainlabs.com>
2023-06-25 17:40:41 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,22 +23,22 @@ fi
 orig_dir=$(pwd)
 
 #create certificates directory
-mkdir -p /usr/local/share/certificates
+mkdir -p $GITHUB_WORKSPACE
 
-# save ca.crt, cert.key, and cert.cert to /usr/local/share/certificates
+# save ca.crt, cert.key, and cert.cert to $GITHUB_WORKSPACE
 if [[ $CHARTMUSEUM_CA_CRT ]]; then
-  echo "CA_CRT is set. Saving to /usr/local/share/certificates/ca.crt"
-  echo $CHARTMUSEUM_CA_CRT | base64 -d > /usr/local/share/certificates/ca.crt
+  echo "CA_CRT is set. Saving to $GITHUB_WORKSPACE/ca.crt"
+  echo $CHARTMUSEUM_CA_CRT | base64 -d > $GITHUB_WORKSPACE/ca.crt
 fi
 
 if [[ $CHARTMUSEUM_KEY ]]; then
-  echo "KEY is set. Saving to /usr/local/share/certificates/cert.key"
-  echo $CHARTMUSEUM_KEY | base64 -d > /usr/local/share/certificates/cert.key
+  echo "KEY is set. Saving to $GITHUB_WORKSPACE/cert.key"
+  echo $CHARTMUSEUM_KEY | base64 -d > $GITHUB_WORKSPACE/cert.key
 fi
 
 if [[ $CHARTMUSEUM_CERT ]]; then
-  echo "CERT is set. Saving to /usr/local/share/certificates/cert.cert"
-  echo $CHARTMUSEUM_CERT | base64 -d > /usr/local/share/certificates/cert.cert
+  echo "CERT is set. Saving to $GITHUB_WORKSPACE/cert.cert"
+  echo $CHARTMUSEUM_CERT | base64 -d > $GITHUB_WORKSPACE/cert.cert
 fi
 
 for CHART_PATH in $PATHS; do
@@ -49,7 +49,7 @@ for CHART_PATH in $PATHS; do
   helm inspect chart .
 
   if [[ $CHARTMUSEUM_REPO_NAME ]]; then
-    helm repo add ${CHARTMUSEUM_REPO_NAME} ${CHARTMUSEUM_URL} --ca-file /usr/local/share/certificates/ca.crt --cert-file /usr/local/share/certificates/cert.crt --key-file /usr/local/share/certificates/cert.key
+    helm repo add ${CHARTMUSEUM_REPO_NAME} ${CHARTMUSEUM_URL} --ca-file $GITHUB_WORKSPACE/ca.crt --cert-file $GITHUB_WORKSPACE/cert.crt --key-file $GITHUB_WORKSPACE/cert.key
   fi
 
   helm dependency update .
@@ -60,7 +60,7 @@ for CHART_PATH in $PATHS; do
 
   export HELM_REPO_ACCESS_TOKEN="${CHARTMUSEUM_JWT}"
   
-  helm cm-push ${CHART_FOLDER}-* ${CHARTMUSEUM_URL} ${FORCE} --ca-file /usr/local/share/certificates/ca.crt --cert-file /usr/local/share/certificates/cert.crt --key-file /usr/local/share/certificates/cert.key
+  helm cm-push ${CHART_FOLDER}-* ${CHARTMUSEUM_URL} ${FORCE} --ca-file $GITHUB_WORKSPACE/ca.crt --cert-file $GITHUB_WORKSPACE/cert.crt --key-file $GITHUB_WORKSPACE/cert.key
 
   # Return to the original working directory at the end of each loop iteration
   cd $orig_dir
