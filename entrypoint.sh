4a44072a141428d7e44b0e0c0e49762e225f156a
a-thomas-22 <athomas@offchainlabs.com>
2023-06-25 17:13:52 -0500

    switch to cert auth

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,16 +13,6 @@ if [ -z "$CHARTMUSEUM_URL" ]; then
   exit 1
 fi
 
-if [ -z "$CHARTMUSEUM_USER" ]; then
-  echo "CHARTMUSEUM_USER is not set. Quitting."
-  exit 1
-fi
-
-if [ -z "$CHARTMUSEUM_PASSWORD" ]; then
-  echo "CHARTMUSEUM_PASSWORD is not set. Quitting."
-  exit 1
-fi
-
 if [ -z "$FORCE" ]; then
   FORCE=""
 elif [ "$FORCE" == "1" ] || [ "$FORCE" == "True" ] || [ "$FORCE" == "TRUE" ]; then
@@ -32,6 +22,19 @@ fi
 # Store the original working directory
 orig_dir=$(pwd)
 
+# save ca.crt, cert.key, and cert.cert to /usr/local/share/certificates
+if [[ $CHARTMUSEUM_CA_CRT ]]; then
+  echo $CHARTMUSEUM_CA_CRT | base64 -d > /usr/local/share/certificates/ca.crt
+fi
+
+if [[ $CHARTMUSEUM_KEY ]]; then
+  echo $CHARTMUSEUM_KEY | base64 -d > /usr/local/share/certificates/cert.key
+fi
+
+if [[ $CHARTMUSEUM_CERT ]]; then
+  echo $CHARTMUSEUM_CERT | base64 -d > /usr/local/share/certificates/cert.cert
+fi
+
 for CHART_PATH in $PATHS; do
   cd $CHART_PATH
 
@@ -40,7 +43,7 @@ for CHART_PATH in $PATHS; do
   helm inspect chart .
 
   if [[ $CHARTMUSEUM_REPO_NAME ]]; then
-    helm repo add ${CHARTMUSEUM_REPO_NAME} ${CHARTMUSEUM_URL} --username=${CHARTMUSEUM_USER} --password=${CHARTMUSEUM_PASSWORD}
+    helm repo add ${CHARTMUSEUM_REPO_NAME} ${CHARTMUSEUM_URL} --ca-file /usr/local/share/ca-certificates/ca.crt --cert-file /usr/local/share/certificates/cert.crt --key-file /usr/local/share/certificates/cert.key
   fi
 
   helm dependency update .
@@ -48,8 +51,10 @@ for CHART_PATH in $PATHS; do
   helm package .
 
   CHART_FOLDER=$(basename "$CHART_PATH")
+
+  export HELM_REPO_ACCESS_TOKEN="${CHARTMUSEUM_JWT}"
   
-  helm cm-push ${CHART_FOLDER}-* ${CHARTMUSEUM_URL} -u ${CHARTMUSEUM_USER} -p ${CHARTMUSEUM_PASSWORD} ${FORCE}
+  helm cm-push ${CHART_FOLDER}-* ${CHARTMUSEUM_URL} ${FORCE} --ca-file /usr/local/share/ca-certificates/ca.crt --cert-file /usr/local/share/certificates/cert.crt --key-file /usr/local/share/certificates/cert.key
 
   # Return to the original working directory at the end of each loop iteration
   cd $orig_dir
