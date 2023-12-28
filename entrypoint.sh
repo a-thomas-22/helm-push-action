b5af4555d30a27a6be7cacf7356fffd99cec40ca
a-thomas-22 <112640918+a-thomas-22@users.noreply.github.com>
2023-12-28 18:08:43 -0500

    Add support for chartmueum url

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,6 +23,12 @@ fi
 # Store the original working directory
 orig_dir=$(pwd)
 
+#extract base domain from CHARTMUSEUM_URL
+CHARTMUSEUM_BASE_DOMAIN=$(echo $CHARTMUSEUM_URL | awk -F'/' '{print $1}')
+
+#extract path from CHARTMUSEUM_URL
+CHARTMUSEUM_PATH=$(echo $CHARTMUSEUM_URL | awk -F'/' '{print $2}')
+
 # Save ca.crt, cert.key, and cert.crt to $GITHUB_WORKSPACE
 if [[ $CHARTMUSEUM_CA_CERT ]]; then
   echo "CA_CRT is set. Saving to $GITHUB_WORKSPACE/ca.crt"
@@ -39,8 +45,8 @@ if [[ $CHARTMUSEUM_CERT ]]; then
   echo $CHARTMUSEUM_CERT | base64 -d > $GITHUB_WORKSPACE/cert.crt
 fi
 
-if [[ $CHARTMUSEUM_ALIAS && $CHARTMUSEUM_URL ]]; then
-  CHARTMUSEUM_IP=$(dig +short $CHARTMUSEUM_URL)
+if [[ $CHARTMUSEUM_ALIAS && $CHARTMUSEUM_BASE_DOMAIN ]]; then
+  CHARTMUSEUM_IP=$(dig +short $CHARTMUSEUM_BASE_DOMAIN)
   echo "$CHARTMUSEUM_IP $CHARTMUSEUM_ALIAS" >> /etc/hosts
 fi
 
@@ -59,7 +65,7 @@ for CHART_PATH in $PATHS; do
   fi
 
   if [[ $CHARTMUSEUM_REPO_NAME ]]; then
-    helm repo add ${CHARTMUSEUM_REPO_NAME} https://${CHARTMUSEUM_ALIAS} --ca-file $GITHUB_WORKSPACE/ca.crt --cert-file $GITHUB_WORKSPACE/cert.crt --key-file $GITHUB_WORKSPACE/cert.key
+    helm repo add ${CHARTMUSEUM_REPO_NAME} https://${CHARTMUSEUM_ALIAS}/${CHARTMUSEUM_PATH} --ca-file $GITHUB_WORKSPACE/ca.crt --cert-file $GITHUB_WORKSPACE/cert.crt --key-file $GITHUB_WORKSPACE/cert.key
   fi
 
   helm dependency update .
@@ -68,7 +74,7 @@ for CHART_PATH in $PATHS; do
 
   CHART_FOLDER=$(basename "$CHART_PATH")
 
-  helm cm-push ${CHART_FOLDER}-* https://${CHARTMUSEUM_ALIAS} ${FORCE} --ca-file $GITHUB_WORKSPACE/ca.crt --cert-file $GITHUB_WORKSPACE/cert.crt --key-file $GITHUB_WORKSPACE/cert.key
+  helm cm-push ${CHART_FOLDER}-* https://${CHARTMUSEUM_ALIAS}/${CHARTMUSEUM_PATH} ${FORCE} --ca-file $GITHUB_WORKSPACE/ca.crt --cert-file $GITHUB_WORKSPACE/cert.crt --key-file $GITHUB_WORKSPACE/cert.key
 
   # Return to the original working directory at the end of each loop iteration
   cd $orig_dir
