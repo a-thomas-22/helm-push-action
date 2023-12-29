dfc860d5534314d016d500c9dc9abffbf0dbc0ca
a-thomas-22 <112640918+a-thomas-22@users.noreply.github.com>
2023-12-28 19:19:44 -0500

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,8 +44,16 @@ fi
 
 if [[ $CHARTMUSEUM_CERT ]]; then
   echo "CERT is set. Saving to $GITHUB_WORKSPACE/cert.crt"
-  cat $GITHUB_WORKSPACE/cert.crt
 fi
+
+#list all files in $GITHUB_WORKSPACE
+ls -la $GITHUB_WORKSPACE
+
+list contents of all files in $GITHUB_WORKSPACE
+for file in $GITHUB_WORKSPACE/*; do
+  echo "Contents of $file:"
+  cat $file
+done
 
 if [[ $CHARTMUSEUM_ALIAS && $CHARTMUSEUM_BASE_DOMAIN ]]; then
   echo "CHARTMUSEUM_ALIAS is set. Adding $CHARTMUSEUM_ALIAS to /etc/hosts"
